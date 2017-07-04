c6d1fc78129ca24c6f6bc55882a66f1843a5bf39
lourenco-cunha <lourenco.cunha13@gmail.com>
2017-07-04 23:50:41 +0100

    add application properties file. add keys to use with woocommerce

---
+++ modules/properties.js
@@ -0,0 +1,18 @@
+
+const extend = require('lodash/extend');
+const path = require('path');
+const properties = require ("properties");
+const props = {};
+
+properties.parse(path.join(__dirname, '../config/application.properties'),
+    {
+        path: true,
+        sections: true
+    },
+    function(error, obj) {
+        if (error) return console.error (error);
+        extend(props, obj);
+});
+
+module.exports = props;
+
