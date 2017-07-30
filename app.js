5bf34147cb107d735afdfc0674ce2ac7a11ca01d
lourenco-cunha <lourenco.cunha13@gmail.com>
2017-07-31 00:04:14 +0100

    use const where possible application bootstrap: - register services endpoint - remove x-powered-by header

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+'use strict';
 
-var app = express();
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const services = require('./routes/services');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('x-powered-by', false);
+//app.set('json spaces', 2);
 
 // uncomment after placing favicon in /web
 //app.use(favicon(path.join(__dirname, 'web', 'favicon.ico')));
@@ -19,9 +25,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'web')));
+app.use(express.static(path.join(__dirname, 'web'), {index:false}));
 
 app.use('/', index);
+app.use('/services', services);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
